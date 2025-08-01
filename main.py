e7497385fe1610af01f608ceb376e0233e717e1a
katyrr <krrobson9@gmail.com>
2025-08-01 22:24:46 +0100

    feat: count num words

--- main.py
+++ main.py
@@ -18,6 +18,24 @@ def get_book_text(filepath):
     return contents
 
 
+def get_num_words(text):
+    '''
+    INPUTS
+    ======
+    - text : string
+        A single string to be analysed.
+
+    OUTPUTS
+    =======
+    - num_words : int
+        The number of words contained in <text>, where words are distinguished by delimeter " ".
+    '''
+
+    split_string = text.split()
+    num_words = len(split_string)
+
+    return num_words
+
 def main():
     '''
     Read the text file "frankenstein" and print the full contents to the console
--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
-from main import get_book_text
+from main import *
 
 def test_func():
 
     test_contents = get_book_text("books/test_text.txt")
+    num_words_in_test = get_num_words(test_contents)
 
-    print(test_contents)
+    print(num_words_in_test)
+    
 
 test_func()
